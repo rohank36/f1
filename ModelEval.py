200cffac88b8e1a44484b1838c5d53479ad92f23
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-05-21 22:42:59 -0400

    Model Eval code

--- ModelEval.py
+++ ModelEval.py
@@ -4,18 +4,12 @@ from Model_v1 import Model_v1
 import Model
 import Dataset 
 import pandas as pd
+from sklearn.metrics import f1_score
 
 """ WORK IN PROGRESS """
 
 class ModelEval():
     def __init__(self, model: Model, dataset: Dataset):
-        """
-        Initialize ModelEval with model and dataset.
-        
-        Args:
-            model: Trained model instance
-            dataset: Dataset instance
-        """
         if model.x_val is None or model.y_val is None:
             raise Exception("Model has no validation data - This class is only meant for exploration during training")
 
@@ -55,6 +49,83 @@ class ModelEval():
         self.pretty_df = pd.concat([trn_df, val_df], axis=0)
         self.pretty_df = self.pretty_df.sort_values(['Year','Round_Number','Probability'],ascending=[True,True,False])
 
-    
     def get_df(self) -> pd.DataFrame:
         return self.pretty_df.copy()
+
+    def get_driver_with_most_wrong_predictions(self):
+        acc_per_driver = self.get_acc_per_driver()
+        worst_idx = acc_per_driver['accuracy'].idxmin()
+        worst_row = acc_per_driver.loc[worst_idx]
+        driver = worst_row['Driver']
+        driver_acc = worst_row['accuracy']
+        print(f"Driver with the most wrong predictions: {driver} {driver_acc}")
+        return self.get_preds_for_driver(driver)
+
+    def get_drivers_with_most_predictions(self):
+        top1_df = self.pretty_df[self.pretty_df['Predicted'] == 1]
+        pred_top_count_per_driver = (
+            top1_df
+            .groupby('Driver')["Predicted"]
+            .count()
+            .reset_index(name='count')
+        )
+        return pred_top_count_per_driver.sort_values(by='count',ascending=False)
+
+    def get_drivers_with_most_predictions_by_year(self, year: int):
+        top1_df = self.pretty_df[(self.pretty_df['Predicted'] == 1) & (self.pretty_df['Year'] == year)]
+        pred_top_count_per_driver = (
+            top1_df
+            .groupby('Driver')["Predicted"]
+            .count()
+            .reset_index(name='count')
+        )
+        return pred_top_count_per_driver.sort_values(by='count',ascending=False)
+
+    def get_acc_per_driver(self):
+        self.pretty_df['correct'] = ( self.pretty_df['Predicted'] == self.pretty_df['Actual'])
+        acc_per_driver = (
+            self.pretty_df
+            .groupby('Driver')['correct']
+            .mean()
+            .reset_index(name='accuracy')
+        )
+        return acc_per_driver.sort_values(by='accuracy')
+
+    def get_preds_for_race(self,year,round_number):
+        return self.pretty_df.loc[(self.pretty_df["Round_Number"]==round_number)&(self.pretty_df["Year"]==year),:]
+    
+    def get_preds_for_driver(self,driver_name):
+        return self.pretty_df.loc[self.pretty_df["Driver"]==driver_name,:]
+
+    def get_f1score_per_driver(self):
+        f1_per_driver = self.pretty_df.groupby('Driver').apply(
+        lambda group: f1_score(
+            group['Actual'], 
+            group['Predicted'], 
+            )
+        ).reset_index(name='f1')
+    
+        return f1_per_driver.sort_values(by='f1', ascending=False)
+
+    
+    def get_f1score_per_race(self):
+        round_to_country = (
+            self.pretty_df
+            .groupby('Round_Number')['Location']
+            .first()
+            .to_dict()
+        )
+
+        f1_per_rd = self.pretty_df.groupby('Round_Number').apply(
+            lambda group: f1_score(
+                group['Actual'], 
+                group['Predicted'], 
+            )
+        ).reset_index(name='f1')
+
+        f1_per_rd['Location'] = f1_per_rd['Round_Number'].map(round_to_country)
+
+        return f1_per_rd.sort_values(by='f1',ascending=False)
+        
+        return f1_per_rd
+    
